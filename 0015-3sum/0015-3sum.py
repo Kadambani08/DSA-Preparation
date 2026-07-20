a33cb26d534dcebce6241e5b2206c2d980084574
Kadambani <207203001+Kadambani08@users.noreply.github.com>
2026-07-20 18:47:35 +0530

    Time: 728 ms (75.03%), Space: 19 MB (14.53%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        result = []
+
+        for i in range(len(nums)-2):
+
+            # skip duplicate fixed numbers
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i + 1
+            right = len(nums)-1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    # need a bigger sum
+                    left += 1
+
+                elif total > 0:
+                    # need a smaller sum
+                    right -= 1
+
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # skip duplicate left values
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+
+                    # skip duplicate right values
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+
+        return result
